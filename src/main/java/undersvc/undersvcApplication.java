47c8c8acecc624610a105110c77ab7feaa5e66c0
Cian Johnston <public@cianjohnston.ie>
2020-01-21 21:24:25 +0000

    initial commit

---
+++ src/main/java/undersvc/undersvcApplication.java
@@ -0,0 +1,29 @@
+package undersvc;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class undersvcApplication extends Application<undersvcConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new undersvcApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "undersvc";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<undersvcConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final undersvcConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/undersvc/undersvcConfiguration.java
@@ -0,0 +1,10 @@
+package undersvc;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class undersvcConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
